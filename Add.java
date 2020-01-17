e9dc4246ca05e6adb446aa270c50fe730999d391
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2020-01-17 23:49:12 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Add
+{
+    public static void main(String args[])
+    {
+    int c=0;    
+    int a=Integer.parseInt(args[0]);
+    int b=Integer.parseInt(args[1]);
+    c=a+b;    
+    System.out.println("Sum of a and b is "+c);
+    }
+} 
+
